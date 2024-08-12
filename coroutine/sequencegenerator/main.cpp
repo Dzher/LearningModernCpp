d393c46d5c2775cec22b68be1da4a1a5e6fe1f9a
Dzher <2539925009@qq.com>
2024-08-12 15:34:03 +0800

    fixup exception in generator, and add fibonacci func

--- coroutine/sequencegenerator/main.cpp
+++ coroutine/sequencegenerator/main.cpp
@@ -10,11 +10,51 @@ Generator sequencegenerator()
     }
 }
 
+Generator fibonacci()
+{
+    co_yield 0;
+    co_yield 1;
+
+    int a = 0;
+    int b = 1;
+    int sum;
+    while (true)
+    {
+        sum = a + b;
+        co_yield sum;
+        a = b;
+        b = sum;
+    }
+}
+
 int main()
 {
     auto gen = sequencegenerator();
-    for (int index = 0; index < 5; ++index)
+    for (int index = 0; index < 15; ++index)
     {
-        std::cout << gen.next() << std::endl;
+        if (gen.hasNext())
+        {
+            std::cout << gen.next() << std::endl;
+        }
+        else
+        {
+            break;
+        }
     }
+
+    std::cout << std::endl;
+
+    auto gen1 = fibonacci();
+    for (int index = 0; index < 15; ++index)
+    {
+        if (gen1.hasNext())
+        {
+            std::cout << gen1.next() << std::endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
 }
--- coroutine/sequencegenerator/sequencegenerator.h
+++ coroutine/sequencegenerator/sequencegenerator.h
@@ -1,5 +1,6 @@
 #include <coroutine>
-
+#include <exception>
+#include <utility>
 struct Generator
 {
     struct promise_type
@@ -18,6 +19,15 @@ struct Generator
         std::suspend_always await_transform(int value)
         {
             this->value = value;
+            is_ready = true;
+            return {};
+        }
+
+        // co_yield expr => co_await promise.yield_value(expr)
+        std::suspend_always yield_value(int value)
+        {
+            this->value = value;
+            is_ready = true;
             return {};
         }
 
@@ -35,12 +45,62 @@ struct Generator
         }
 
         int value;
+        bool is_ready = false;
     };
 
-    std::coroutine_handle<promise_type> handle;
+    explicit Generator(std::coroutine_handle<promise_type> handle) noexcept : handle_(handle)
+    {
+    }
+
+    Generator(Generator&& generator) noexcept : handle_(std::exchange(generator.handle_, {}))
+    {
+    }
+
+    Generator(Generator&) = delete;
+    Generator& operator=(Generator&) = delete;
+
+    ~Generator()
+    {
+        if (handle_)
+        {
+            handle_.destroy();
+        }
+    }
+
     int next()
     {
-        handle.resume();
-        return handle.promise().value;
+        if (hasNext())
+        {
+            handle_.promise().is_ready = false;
+            return handle_.promise().value;
+        }
+        throw ExhaustedException{};
     }
+
+    bool hasNext()
+    {
+        if (!handle_ || handle_.done())
+        {
+            return false;
+        }
+
+        if (!handle_.promise().is_ready)
+        {
+            handle_.resume();
+        }
+
+        if (handle_.done())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // after coroutine executed, invoke form outside throw exception
+    class ExhaustedException : std::exception
+    {
+    };
+
+    std::coroutine_handle<promise_type> handle_;
 };
