cf03c117e2629465c917d8ca14c34c095a04cc97
Dzher <2539925009@qq.com>
2024-08-22 15:05:32 +0800

    add another async time method

--- asio_test/timer_async.cpp
+++ asio_test/timer_async.cpp
@@ -1,5 +1,6 @@
 #include <functional>
 #include <iostream>
+#include <system_error>
 #include "asio.hpp"
 #include "asio/detail/chrono.hpp"
 #include "asio/io_context.hpp"
@@ -9,37 +10,76 @@ void printWithParams(const std::error_code&, asio::steady_timer* timer, int& cou
 {
     if (count < 5)
     {
-        std::cout << "count is " << count++ << std::endl;
+        std::cout << "Count is " << count++ << std::endl;
         timer->expires_at(timer->expiry() + asio::chrono::seconds(1));
         timer->async_wait(std::bind(printWithParams, asio::placeholders::error, timer, count));
     }
     else
     {
-        std::cout << "Finished" << std::endl;
+        std::cout << "Method 2 Finished" << std::endl;
+        std::cout << std::endl;
     }
 }
 
+class Printer
+{
+public:
+    explicit Printer(asio::io_context& io) : timer_(io, asio::chrono::seconds(1)), count_(0)
+    {
+        timer_.async_wait(std::bind(&Printer::print, this));
+    }
+    ~Printer()
+    {
+        std::cout << "Method 3 Finished" << std::endl;
+        std::cout << std::endl;
+    }
+
+    void print()
+    {
+        if (count_ < 5)
+        {
+            std::cout << "Count is " << count_++ << std::endl;
+            timer_.expires_at(timer_.expiry() + asio::chrono::seconds(1));
+            timer_.async_wait(std::bind(&Printer::print, this));
+        }
+    }
+
+private:
+    asio::steady_timer timer_;
+    int count_;
+};
+
 int main()
 {
+    // method 1
     asio::io_context io;
     asio::steady_timer t{io, asio::chrono::seconds{5}};
 
     auto print = [](const std::error_code&)
     {
-        std::cout << "5s Timeout" << std::endl;
+        std::cout << "Method 1 Finished" << std::endl;
+        std::cout << std::endl;
     };
 
-    std::cout << "Start..." << std::endl;
+    std::cout << "Start method 1..." << std::endl;
     t.async_wait(print);
-    std::cout << "Do something else..." << std::endl;
+    std::cout << "Wait for method 1..." << std::endl;
     io.run();
 
-    std::cout << "Continue..." << std::endl;
+    // method 2
+    std::cout << "Start method 2..." << std::endl;
     asio::io_context io1;
     int count = 0;
     asio::steady_timer timer{io1, asio::chrono::seconds(1)};
     timer.async_wait(std::bind(printWithParams, asio::placeholders::error, &timer, count));
+    std::cout << "Wait for method 2..." << std::endl;
     io1.run();
 
+    // method 3
+    std::cout << "Start method 3..." << std::endl;
+    asio::io_context io2;
+    Printer printer(io2);
+    std::cout << "Wait for method 3..." << std::endl;
+    io2.run();
     return 0;
 }
