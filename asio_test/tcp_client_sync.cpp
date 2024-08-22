8dd48041f1f20b911a302d6fe9a1cb97eaaae939
Dzher <2539925009@qq.com>
2024-08-22 16:33:02 +0800

    add tcp client and server

---
+++ asio_test/tcp_client_sync.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "asio.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Error: Host issue" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        asio::io_context io_context;
+
+        asio::ip::tcp::resolver resolver(io_context);
+        asio::ip::tcp::resolver::results_type endpoints = resolver.resolve(argv[1], "daytime");
+
+        asio::ip::tcp::socket socket(io_context);
+        asio::connect(socket, endpoints);
+
+        while (1)
+        {
+            std::array<char, 128> buf;
+            std::error_code error;
+
+            size_t len = socket.read_some(asio::buffer(buf), error);
+
+            if (error == asio::error::eof)
+            {
+                break;  // Connection closed cleanly by peer.
+            }
+            else if (error)
+            {
+                throw std::system_error(error);  // Some other error.
+            }
+            std::cout.write(buf.data(), len);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ asio_test/tcp_server_sync.cpp
@@ -0,0 +1,40 @@
+#include <ctime>
+#include <iostream>
+#include "asio.hpp"
+#include "asio/detail/chrono.hpp"
+#include "asio/steady_timer.hpp"
+
+std::string getDaytime()
+{
+    std::time_t now = std::time(0);
+    return std::ctime(&now);
+}
+
+int main()
+{
+    try
+    {
+        asio::io_context io_context;
+
+        asio::ip::tcp::acceptor acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 13));
+
+        asio::ip::tcp::socket socket(io_context);
+        acceptor.accept(socket);
+
+        asio::steady_timer timer{io_context, asio::chrono::seconds(1)};
+        while (1)
+        {
+            std::string message = getDaytime();
+            std::error_code ignored_error;
+            asio::write(socket, asio::buffer(message), ignored_error);
+            timer.wait();
+            timer.expires_at(timer.expiry() + asio::chrono::seconds(1));
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
