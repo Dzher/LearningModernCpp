d61860a17b80104815abdde2868985b27f38a73b
Dzher <2539925009@qq.com>
2024-08-25 17:28:29 +0800

    add udp sync client and server

---
+++ asio_test/udp_client_sync.cpp
@@ -0,0 +1,40 @@
+#include <array>
+#include <exception>
+#include <iostream>
+#include "asio/io_context.hpp"
+#include "asio/ip/udp.hpp"
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Error: Host Issue!" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        asio::io_context context;
+
+        asio::ip::udp::resolver resolver{context};
+        asio::ip::udp::endpoint receiver_endpoint =
+            resolver.resolve(asio::ip::udp::v4(), argv[1], "daytime").begin()->endpoint();
+
+        std::array<char, 1> send_buf = {{0}};
+        asio::ip::udp::socket socket{context};
+        socket.open(asio::ip::udp::v4());
+        socket.send_to(asio::buffer(send_buf), receiver_endpoint);
+
+        std::array<char, 128> recv_buf;
+        asio::ip::udp::endpoint sender_endpoint;
+        size_t len = socket.receive_from(asio::buffer(recv_buf), sender_endpoint);
+
+        std::cout.write(recv_buf.data(), len);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ asio_test/udp_server_sync.cpp
@@ -0,0 +1,42 @@
+#include <array>
+#include <ctime>
+#include <exception>
+#include <iostream>
+#include <string>
+#include <system_error>
+#include "asio/buffer.hpp"
+#include "asio/io_context.hpp"
+#include "asio/ip/udp.hpp"
+
+std::string getDaytime()
+{
+    std::time_t now = std::time(0);
+    return std::ctime(&now);
+}
+
+int main()
+{
+    try
+    {
+        asio::io_context io_context;
+        asio::ip::udp::socket socket(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), 13));
+
+        while (1)
+        {
+            std::array<char, 1> recv_buf;
+            asio::ip::udp::endpoint remote_endpoint;
+            socket.receive_from(asio::buffer(recv_buf), remote_endpoint);
+
+            std::string message = getDaytime();
+
+            std::error_code ignored_error;
+            socket.send_to(asio::buffer(message), remote_endpoint, 0, ignored_error);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
