4ad18906d19978425441138024a58560b8b12f19
Dzher <2539925009@qq.com>
2024-09-03 17:40:54 +0800

    add udp echo code

---
+++ asio_test/echo/udp_echo_client.cpp
@@ -0,0 +1,44 @@
+#include <cstddef>
+#include <exception>
+#include <iostream>
+#include "asio/io_context.hpp"
+#include "asio/ip/udp.hpp"
+
+const int max_length = 1024;
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Error: client <host> <port>" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        asio::io_context io_context;
+        asio::ip::udp::resolver resolver{io_context};
+        auto endpoint = resolver.resolve(asio::ip::udp::v4(), argv[1], argv[2])->endpoint();
+        asio::ip::udp::socket socket{io_context};
+        socket.open(asio::ip::udp::v4());
+
+        std::cout << "Enter Message: " << std::endl;
+        char send_message[max_length];
+        std::cin.getline(send_message, max_length);
+        size_t send_len = std::strlen(send_message);
+        socket.send_to(asio::buffer(send_message, send_len), endpoint);
+
+        char reveive_message[max_length];
+        asio::ip::udp::endpoint sender_endpoint;
+        size_t receive_length = socket.receive_from(asio::buffer(reveive_message, max_length), sender_endpoint);
+        std::cout << "Reply is: ";
+        std::cout.write(reveive_message, receive_length);
+        std::cout << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ asio_test/echo/udp_echo_server.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "asio/io_context.hpp"
+#include "asio/ip/udp.hpp"
+
+const int max_length = 1024;
+
+void server(asio::io_context& io_context, unsigned short port)
+{
+    asio::ip::udp::socket sock(io_context, asio::ip::udp::endpoint(asio::ip::udp::v4(), port));
+    for (;;)
+    {
+        char data[max_length];
+        asio::ip::udp::endpoint sender_endpoint;
+        size_t length = sock.receive_from(asio::buffer(data, max_length), sender_endpoint);
+        sock.send_to(asio::buffer(data, length), sender_endpoint);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: blocking_udp_echo_server <port>" << std::endl;
+            return 1;
+        }
+
+        asio::io_context io_context;
+        server(io_context, std::atoi(argv[1]));
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
