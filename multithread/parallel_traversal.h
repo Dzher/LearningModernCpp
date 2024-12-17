491c85fc40553501130e58c7dea54858cfe54d5b
Dzher <2539925009@qq.com>
2024-12-17 17:06:46 +0800

    compare diy paraller and std for_each

--- multithread/parallel_traversal.h
+++ multithread/parallel_traversal.h
@@ -1,9 +1,14 @@
+#include <algorithm>
 #include <future>
 #include <iostream>
 #include <iterator>
 #include <thread>
 
-static constexpr int kMinPerThreadCount = 25;
+// Design Idea:
+// if the task num <= kMinPerThreadCount, just run all tasks in main thread
+// if the task num > kMinPerThreadCount, then distribute the tasks euqally to main and sub threads
+
+static constexpr int kMinPerThreadCount = 25; // must set a suitable value or the efficiency will be bad
 static const unsigned long kHardwareThreadCount = std::thread::hardware_concurrency();
 
 template <typename Iterator, typename Func>
@@ -43,7 +48,7 @@ void paraller_traversal(Iterator first, Iterator last, Func func) {
 }
 
 int main() {
-    int task_num = 101;
+    int task_num = 50000000;
     std::vector<int> funcs(task_num, 1);
 
     static long value = 0;
@@ -52,6 +57,18 @@ int main() {
         value += each_value;
     };
 
+    auto start = std::chrono::high_resolution_clock::now();
     paraller_traversal(funcs.begin(), funcs.end(), sumValue);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     std::cout << "total value is " << value << std::endl;
+    std::cout << "DIY Parallel Elapsed Time: " << duration.count() << " us\n";
+
+    value = 0;
+    start = std::chrono::high_resolution_clock::now();
+    std::for_each(funcs.begin(), funcs.end(), sumValue);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "total value is " << value << std::endl;
+    std::cout << "Std::for_each Elapsed Time: " << duration.count() << " us\n";
 }
